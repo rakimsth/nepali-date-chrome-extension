b0ac7de0bfd8d9ad1f32ce312abe42e54444e356
Raktim Shrestha <rakimsth@gmail.com>
2024-05-25 23:53:03 +0545

    added NAV for NIBL

--- popup.js
+++ popup.js
@@ -12,17 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(async () => {
       const metal = await getGoldPrice();
       const weather = await getWeather();
+      const nav = await getNavPrice();
       const currentWeather = weather?.current?.condition || {
         icon: "",
         text: "-",
       };
       const weatherForecast = weather?.forecast?.forecastday || [];
-      dialogBox.innerHTML = dataToHtml(
+      dialogBox.innerHTML = dataToHtml({
         today,
         currentWeather,
         weatherForecast,
-        metal
-      );
+        metal,
+        nav,
+      });
     }, 1000);
   });
 });
@@ -32,7 +34,7 @@ const metalToHtml = (metal) => {
   <hr>
   <span style="color:#fff;background-color:#FFD700;font-size:0.9em;">
   Gold: ${metal.gold}
-  </span>
+  </span>&nbsp;|
   <span style="color:#fff;background-color:#C0C0C0;font-size:0.9em;">
   Silver: ${metal.silver}
   </span>
@@ -103,12 +105,14 @@ const weatherToHtml = ({ currentWeather, weatherForecast }) => {
   }
 };
 
-const dataToHtml = (today, currentWeather, weatherForecast, metal) => {
+const dataToHtml = ({ today, currentWeather, weatherForecast, metal, nav }) => {
   let toPrint = "";
+  // Today
   if (today) {
     const todayData = todayToHtml(today);
     toPrint += todayData;
   }
+  // Gold Data
   if (metal) {
     const metalData = metalToHtml(metal);
     toPrint += metalData;
@@ -118,6 +122,7 @@ const dataToHtml = (today, currentWeather, weatherForecast, metal) => {
         <span><em>loading gold data...</em></span>
       </div>`;
   }
+  // Weather Data
   if (currentWeather && weatherForecast) {
     const weatherData = weatherToHtml({ currentWeather, weatherForecast });
     toPrint += weatherData;
@@ -127,7 +132,39 @@ const dataToHtml = (today, currentWeather, weatherForecast, metal) => {
         <span><em>loading weather data...</em></span>
         </div>`;
   }
+  if (nav) {
+    const navData = navToHtml(nav);
+    toPrint += navData;
+  } else {
+    toPrint += `
+      <div style="margin-top: 25px">
+        <span><em>loading NAV data...</em></span>
+      </div>`;
+  }
   return toPrint;
+};
+
+const navToHtml = (nav) => {
+  return `
+  <hr>
+  <span style="color:#fff;background-color:${nav?.color};font-size:0.9em;">
+  NAV: ${nav?.nav}
+  </span>|
+  <span style="font-size:0.9em;">
+  Difference: ${nav?.difference}
+  </span>|
+  <span style="font-size:0.9em;">
+  %: ${nav?.diffPercent}
+  </span>
+  <a href="https://www.nimbacecapital.com/mutual-fund/nav-nibl-sahabhagita-fund" target="_blank">></a>
+  <br>
+  <div style="font-size: 0.7em;text-align:right;">
+  <i>
+  Last Updated at: <strong>${nav.fetchDate}</strong> (location: Nepal)
+  </i>
+  </div>
+  <hr>
+  `;
 };
 
 const getNepToday = () => {
@@ -201,4 +238,59 @@ async function getGoldPrice() {
   const additionalSPIndex = searchSilverIndex + 13;
   let silverPriceData = metals.substring(searchSilverIndex, additionalSPIndex);
   return { fetchDate: dateData, gold: goldPriceData, silver: silverPriceData };
+}
+
+async function getNavPrice() {
+  const response = await fetchTimeout(
+    `https://www.nimbacecapital.com/mutual-fund/nav-nibl-sahabhagita-fund`,
+    3000,
+    {
+      signal: controller.signal,
+    }
+  ).catch((error) => {
+    console.log(error);
+  });
+
+  if (!response)
+    return {
+      fetchDate: "-",
+      nav: "-",
+      difference: "-",
+      diffPercent: "-",
+      color: "-",
+    };
+  const navData = await response.text();
+  // Getting NAV Fetch Date
+  const fetchIndex = navData.search(/nibl-table id=nav>/gi);
+  const dateSearchIndex = fetchIndex + 91;
+  const additionalIndex = dateSearchIndex + 10;
+  const date = navData.substring(dateSearchIndex, additionalIndex);
+
+  // Getting NAV Color Data
+  const colorSearchIndex = fetchIndex + 121;
+  const additionalColorIndex = colorSearchIndex + 9;
+  const colorLabel = navData.substring(colorSearchIndex, additionalColorIndex);
+  const color = colorLabel.split("-")[1];
+
+  // Getting NAV Data
+  const navSearchIndex = fetchIndex + 147;
+  const additionalNavIndex = navSearchIndex + 5;
+  const nav = navData.substring(navSearchIndex, additionalNavIndex);
+
+  // Getting NAV Diff Data
+  const differenceSearchIndex = fetchIndex + 205;
+  const additionalDiffIndex = differenceSearchIndex + 4;
+  const difference = navData.substring(
+    differenceSearchIndex,
+    additionalDiffIndex
+  );
+
+  // Getting NAV Percentage Data
+  const differencePerSearchIndex = fetchIndex + 270;
+  const additionalDiffPerIndex = differencePerSearchIndex + 5;
+  const diffPer = navData.substring(
+    differencePerSearchIndex,
+    additionalDiffPerIndex
+  );
+  return { fetchDate: date, nav, difference, diffPercent: diffPer, color };
 }
